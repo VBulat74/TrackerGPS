554224322366811706c7b1e3c5068128c1a3aa40
VB <Vladimir.Bulat@gmail.com>
2023-08-07 16:34:51 +0300

    FusedLocationUpdate

--- app/src/main/java/ru/com/bulat/trackergps/location/LocationService.kt
+++ app/src/main/java/ru/com/bulat/trackergps/location/LocationService.kt
@@ -1,39 +1,53 @@
 package ru.com.bulat.trackergps.location
 
+import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.IBinder
+import android.os.Looper
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.Granularity
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import ru.com.bulat.trackergps.MainActivity
 import ru.com.bulat.trackergps.R
 
 class LocationService : Service() {
+
+    private lateinit var locProvider : FusedLocationProviderClient
+    private lateinit var locRequest : LocationRequest
+
     override fun onBind(p0: Intent?): IBinder? {
         return null
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.d("AAA","LocalService onStartCommand: isRunning = $isRunning; startTime = ${startTime}")
         startNotification()
+        startLocationUpdate()
         isRunning = true
         return START_STICKY
     }
 
     override fun onCreate() {
-        Log.d("AAA","LocalService onCreate isRunning = $isRunning; startTime = $startTime")
         super.onCreate()
-
+        initLocation()
     }
 
     override fun onDestroy() {
-        Log.d("AAA","LocalService onDestroy isRunning = $isRunning; isRunning = $startTime")
         super.onDestroy()
         isRunning = false
+        locProvider.removeLocationUpdates(locCallBack)
     }
 
 
@@ -64,6 +78,45 @@ class LocationService : Service() {
             .setContentIntent(pIntent)
             .build()
         startForeground(99, notification)
+    }
+
+    private val locCallBack = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+            Log.d("AAA", "Location (${locationResult.lastLocation?.latitude}, ${locationResult.lastLocation?.longitude})")
+        }
+    }
+
+    private fun initLocation(){
+        /*locRequest =  LocationRequest.create().apply {
+            interval = 5000
+            fastestInterval = 5000
+            priority = PRIORITY_HIGH_ACCURACY
+        }*/
+
+
+        locRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 5000).apply {
+            setGranularity(Granularity.GRANULARITY_FINE)
+            setWaitForAccurateLocation(true)
+        }.build()
+
+
+        locProvider = LocationServices.getFusedLocationProviderClient(baseContext)
+    }
+
+    private fun startLocationUpdate(){
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        locProvider.requestLocationUpdates(
+            locRequest,
+            locCallBack,
+            Looper.myLooper()
+        )
     }
 
     companion object {
